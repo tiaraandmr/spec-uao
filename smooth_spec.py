be987914e90a325b1ab00e49e84dc527ebce81c0
Tiara Andamari Saraswati <tiaraandmr@users.noreply.github.com>
2024-07-09 13:45:18 +0200

    Update smooth_spec.py

--- smooth_spec.py
+++ smooth_spec.py
@@ -9,11 +9,9 @@ from matplotlib import rc
 rc('font',**{'family':'serif','serif':['lmodern']})
 rc('text', usetex=True)
 import matplotlib.pyplot as plt
-#from matplotlib.widgets import TextBox
 from astropy.io import fits
 
 clean_spec = '/Users/saraswati/Documents/Work/spec-uao/clean_spec/'
-list_spec = sorted(os.listdir(clean_spec))
 
 smooth_spec = '/Users/saraswati/Documents/Work/spec-uao/smooth_spec/'
 if os.path.exists(smooth_spec):
@@ -27,9 +25,13 @@ species = np.array([['[SII]', 6716.435709204644], ['[SII]', 6730.811841618589], 
                     ['[OII]', 3727.4199178007175], ['HI', 6562.797027356974], ['HI', 4861.321979760415], ['HI', 4340.459677187083], 
                     ['CIV', 1548.8576020874154], ['HeII', 1639.7908206670402], ['CIII]', 1908.1020362243723], ['MgII', 2798.2921038031536]])
 
-for i in range(len(list_spec)):
+slits = Table.read('/Users/saraswati/Documents/Work/spec-uao/slits_reduced.fits')
+object_ID = slits['ID'].astype('str')
+z = slits['BEST_Z']
+
+for i in range(len(object_ID)):
     #read the spectrum
-    spec_data = Table.read(clean_spec+list_spec[i])
+    spec_data = Table.read(clean_spec+object_ID[i]+'.fits')
     wav = spec_data['WAVELENGTH']
     flux = spec_data['FLUX']
     
@@ -46,10 +48,9 @@ for i in range(len(list_spec)):
     ax.plot(wav, spec_convolve, color='firebrick', linewidth=2.0, drawstyle='steps-mid')
     
     #define the approximate redshift (z)
-    slits = Table.read('/Users/saraswati/Documents/Work/spec-uao/slits_reduced.fits')
-    best_z = spec_data['BEST_Z']
-    # check if its the same data as the one loaded on the list_spec
+    best_z = z[i]
 
+    #check if lines are in the range of the observed spectrum
     for i in range(len(species)):
         data = species[i][1].astype('float64')
         x = data*(1+best_z)
@@ -58,19 +59,20 @@ for i in range(len(list_spec)):
             ax.vlines(x, ymin=flux.min(), ymax=flux.max(), color = 'black', ls = '--') 
             ax.text(x, 1.1*flux.max(), species[i][0], rotation=90, fontsize=14, ha='center', va='center')
 
+    #setting the x- and y-axis limit
     ax.set_xlim(wav[good].min(), wav[good].max())
     ax.set_ylim(flux.min(), 1.25*flux.max())
     
     #define x and y label and plot title
     ax.set_xlabel(r'Observed Wavelength [$ \rm \AA$]', labelpad=10)
     ax.set_ylabel(r'Flux [$\mathrm{10^{-17}\ erg\ cm^{-2}\ s^{-1}\ \AA^{-1}}$]', labelpad=10)
-    title = Path(list_spec[i]).stem
+    title = object_ID[i]
     ax.set_title(title+r', $z = {:.3f}$'.format(best_z), pad=20)
 
-    # Rest Frame
+    #plot a rest frame wavelength based on the best_z
     ax2 = ax.secondary_xaxis('top',functions=(lambda lam: lam/(1+best_z), lambda lam: lam*(1+best_z))) 
     ax2.set_xlabel(r'Rest Wavelength [$ \rm \AA$]', labelpad=10)
 
     #save the plot as .pdf
-    plt.savefig(smooth_spec+title+'smooth_n3.pdf', bbox_inches="tight")
+    plt.savefig(smooth_spec+title+'_smooth_n3.pdf', bbox_inches="tight")
     plt.close()
