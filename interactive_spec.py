db0ab8b83082cdab61a7bae5da39506894d9c545
Tiara Andamari Saraswati <tiaraandmr@users.noreply.github.com>
2024-07-17 17:25:02 +0200

    Interactive plot to appoximate redshift value

--- interactive_spec.py
+++ interactive_spec.py
@@ -1,6 +1,6 @@
-import os, glob, shutil, subprocess as sub
-from pathlib import Path
+#!/usr/bin/python python3
 
+# Import packages
 import numpy as np
 from astropy.table import Table
 from astropy.convolution import convolve
@@ -9,18 +9,82 @@ from matplotlib import rc
 rc('font',**{'family':'serif','serif':['lmodern']})
 rc('text', usetex=True)
 import matplotlib.pyplot as plt
-#from matplotlib.widgets import TextBox
+from matplotlib.widgets import TextBox
 from astropy.io import fits
+from spectres import spectres
 
-#def visualizeGraph(expr):
-#    ydata = eval(expr)
-#    spec_convolve.set_ydata(ydata)
-#    ax.relim()
-#    ax.autoscale_view()
-#    plt.draw()
- 
-# Adding TextBox to graph
-#graphBox = fig.add_axes([0.1, 0.05, 0.8, 0.075])
-#txtBox = TextBox(graphBox, r'Plot: ')
-#txtBox.on_submit(visualizeGraph)
-#txtBox.set_val('0.81')
+# Define home directory
+home = '/Users/saraswati/Documents/Work/spec-uao/calibration_star/'
+
+# Load the flux sampling template
+sampling_path = '/Users/saraswati/Documents/Work/spec-uao/calibration_star/BD+33d2642_20230708_sampling.fits'
+sampling = Table.read(sampling_path)
+
+# Load the observed spectrum
+spec_obs_path = '/Users/saraswati/Documents/Work/spec-uao/clean_spec/41231853545355348.fits'
+spec_obs = Table.read(spec_obs_path)
+
+# Create a new wavelength array for both spectra
+new_wav = spec_obs['WAVELENGTH']
+flux = spec_obs['FLUX']
+
+# Resample the spectrum based on the new wavelength array
+flux_sampling_resample = spectres(new_wav, sampling['WAVELENGTH'], sampling['FLUX'], verbose=False)
+
+# Apply the flux sampling to the observed spectrum
+flux_calibrated = spec_obs['FLUX'] * flux_sampling_resample
+
+# Automatically set the x-limit for every spectrum
+good = np.invert(np.isnan(flux_calibrated))
+
+# Smooth the spectra
+N = 5
+spec_convolve = convolve(flux_calibrated, np.ones(N), boundary='extend', normalize_kernel=True)
+spec_convolve_orig = convolve(spec_obs['FLUX'], np.ones(N), boundary='extend', normalize_kernel=True)
+
+# Define line species
+species = np.array([['[SII]', 6716.435709204644], ['[SII]', 6730.811841618589], ['[NII]', 6583.451474377235], ['[NII]', 6548.050991532048], 
+                    ['[OIII]', 5006.843330296288], ['[OIII]', 4958.91106815638], ['[OIII]', 4363.2096874449635], ['[NeIII]', 3868.7632032493057], 
+                    ['[NeV]', 3425.8676240424247], ['[NeV]', 3345.828076914398], ['[OI]', 6300.303551730963], ['[OI]', 6363.776506749638], 
+                    ['[OII]', 3727.4199178007175], ['HI', 6562.797027356974], ['HI', 4861.321979760415], ['HI', 4340.459677187083], 
+                    ['CIV', 1548.8576020874154], ['HeII', 1639.7908206670402], ['CIII]', 1908.1020362243723], ['MgII', 2798.2921038031536]])
+
+# define the figure and font size
+plt.rcParams.update({'font.size': 14})
+fig, ax = plt.subplots(figsize=(17, 5))
+fig.subplots_adjust(hspace=0, wspace=0, bottom=0.2)
+
+ax.plot(new_wav, spec_convolve, color='firebrick', linewidth=2.0, drawstyle='steps-mid', label='Calibrated \& Smoothed (N=5)', alpha=0.7)
+
+#define x and y label and plot title
+ax.set_xlabel(r'Observed Wavelength [$ \rm \AA$]', labelpad=10)
+ax.set_ylabel(r'Flux [$\mathrm{10^{-17}\ erg\ cm^{-2}\ s^{-1}\ \AA^{-1}}$]', labelpad=10)
+
+#setting the x- and y-axis limit
+ax.set_xlim(new_wav[good].min(), new_wav[good].max())
+ax.set_ylim(flux.min(), 0.75*flux.max())
+
+# Plot lines
+lines = [ax.vlines(line, flux.min(), 0.5*flux.max(), color = 'black', ls = '--')
+         for line in species[:, 1].astype('float64')]
+
+# Update function
+def submit(redshift):
+    z = float(redshift)
+
+    # Update line positions
+    for line_obj, line in zip(lines, species[:, 1].astype('float64')):
+        line_obj.set_segments(
+            [[(line * (1 + z), flux.min()), (line * (1 + z), flux.max())]]
+        )
+    
+    ax.relim()
+    ax.autoscale_view()
+    plt.draw()
+
+axbox = fig.add_axes([0.1, 0.05, 0.8, 0.075])
+text_box = TextBox(axbox, 'Redshift', textalignment='center')
+text_box.on_submit(submit)
+text_box.set_val('0.8')  # Trigger `submit` with the initial string.
+
+plt.show()
