757674dc7e9ffbf326d751597fb66d80e9e50405
Tiara Andamari Saraswati <tiaraandmr@users.noreply.github.com>
2024-07-29 13:15:31 +0200

    Update script

--- flux_calib_all.py
+++ flux_calib_all.py
@@ -26,195 +26,71 @@ if os.path.exists(calibrated_spec):
     shutil.rmtree(calibrated_spec)
 os.makedirs(calibrated_spec)
 
-for i in range(len(folder)):
+for f in folder:
+
     #list the folder inside home directory
-    reduced = home+folder[i]+'/reduced/'
+    reduced = home+f+'/reduced/'
     spectra = sorted(os.listdir(reduced))
 
-    if folder[i] == 'Hviding2023A_0_733':
+    if f == 'Hviding2023A_0_733':
+        sampling_path = '/Users/saraswati/Documents/Work/spec-uao/calibration_star/fits_sampling/Feige34_sampling.fits'
+    elif f == 'Hviding2023A_1_734':
+        sampling_path = '/Users/saraswati/Documents/Work/spec-uao/calibration_star/fits_sampling/BD+33d2642_20230415_sampling.fits'
+    elif f == 'Hviding2023A_2_735':
+        sampling_path = '/Users/saraswati/Documents/Work/spec-uao/calibration_star/fits_sampling/BD+33d2642_20230708_sampling.fits'
+    elif f == 'Hviding2023A_4_737':
+        sampling_path = '/Users/saraswati/Documents/Work/spec-uao/calibration_star/fits_sampling/BD+33d2642_20230708_sampling.fits'
+    else:
+        continue
 
-        #list the .FITS file for each folder
-        prefixes = ["slitA001_1", "slitA015_1", "slitB002_1", "slitB011_1"]
-        list_spec = sorted((f for f in os.listdir(reduced+spectra[0]+'/obj_abs_1D/') if not f.startswith(tuple(prefixes))), key=str.lower)
+    #list the .FITS file for each folder
+    prefixes = ["slitA001_1", "slitA015_1", "slitB002_1", "slitB011_1"]
+    list_spec = sorted((f for f in os.listdir(reduced+spectra[0]+'/obj_abs_1D/') if not f.startswith(tuple(prefixes))), key=str.lower)
 
-        for i in range(len(list_spec)):
+    for spec in list_spec:
 
-            # Load the flux sampling template
-            sampling_path = '/Users/saraswati/Documents/Work/spec-uao/calibration_star/Feige34_sampling.fits'
-            sampling = Table.read(sampling_path)
+        # Load the flux sampling template
+        sampling = Table.read(sampling_path)
 
-            # Load the .FITS file
-            hdul = fits.open(reduced+spectra[0]+'/obj_abs_1D/'+list_spec[i])
-            name = hdul[0].header['OBJECT']
-            name_clean = name.rsplit('_', 1)[0]
+        # Load the .FITS file
+        hdul = fits.open(reduced+spectra[0]+'/obj_abs_1D/'+spec)
+        name = hdul[0].header['OBJECT']
+        name_clean = name.rsplit('_', 1)[0]
 
-            # Load the observed spectrum
-            spec_obs_path = '/Users/saraswati/Documents/Work/spec-uao/clean_spec/'+name_clean+'.fits'
-            spec_obs = Table.read(spec_obs_path)
+        # Load the observed spectrum
+        spec_obs_path = '/Users/saraswati/Documents/Work/spec-uao/clean_spec/'+name_clean+'.fits'
+        spec_obs = Table.read(spec_obs_path)
 
-            # create a new wavelength array for both spectra
-            new_wav = spec_obs['WAVELENGTH']
-            flux = spec_obs['FLUX']
+        # create a new wavelength array for both spectra
+        new_wav = spec_obs['WAVELENGTH']
+        flux = spec_obs['FLUX']
+        err = spec_obs['ERR']
 
-            # resample the 'sampling' spectra based on the new wavelength array
-            flux_sampling_resample = spectres(new_wav, sampling['WAVELENGTH'], sampling['FLUX'], verbose=False)
+        # resample the 'sampling' spectra based on the new wavelength array
+        flux_sampling_resample, flux_err_sampling_resample = spectres(new_wav, sampling['WAVELENGTH'], sampling['FLUX'], spec_errs=sampling['ERR'], verbose=False)
 
-            # Apply the flux sampling to the observed spectra
-            flux_calibrated = spec_obs['FLUX'] * flux_sampling_resample
+        # Apply the flux sampling to the observed spectra
+        flux_calibrated = spec_obs['FLUX'] * flux_sampling_resample
+        flux_err_calibrated = flux_calibrated * np.sqrt((flux_err_sampling_resample/flux_sampling_resample)**2 + (spec_obs['ERR']/spec_obs['FLUX'])**2)
 
-            #smooth the spectra
-            N = 5
-            spec_convolve = convolve(flux_calibrated, np.ones(N), boundary='extend', normalize_kernel=True)
+        # Get the mask
+        mask = np.logical_or.reduce([flux_calibrated.mask,np.isnan(flux_calibrated),flux_err_calibrated.mask,np.isnan(flux_err_calibrated)])
 
-            #save spectra into a DataFrame
-            spec_new = pd.DataFrame()
-            spec_new.insert(0, "WAVELENGTH", new_wav)
-            spec_new.insert(1, "FLUX", spec_convolve)
+        #smooth the spectra
+        N = 5
+        # spec_convolve = convolve(flux_calibrated, np.ones(N), boundary='extend', normalize_kernel=True)
+        # spec_err_convolve = convolve(flux_err_calibrated, np.ones(N), boundary='extend', normalize_kernel=True)
 
-            #convert DataFrame into Table
-            spec_tab = Table.from_pandas(spec_new)
+        #save spectra into a DataFrame
+        spec_new = pd.DataFrame()
+        spec_new.insert(0, "WAVE", new_wav)
+        spec_new.insert(1, "FLUX", flux_calibrated)
+        spec_new.insert(2, "ERROR", flux_err_calibrated)
+        spec_new.insert(3, "MASK", mask)
 
-            #save each spectrum into clean_spec directory
-            os.chdir(calibrated_spec)
-            spec_tab.write(name_clean+'.fits')
+        #convert DataFrame into Table
+        spec_tab = Table.from_pandas(spec_new)
 
-    elif folder[i] == 'Hviding2023A_1_734':
-
-        #list the .FITS file for each folder
-        prefixes = ["slitA001_1", "slitA015_1", "slitB002_1", "slitB011_1"]
-        list_spec = sorted((f for f in os.listdir(reduced+spectra[0]+'/obj_abs_1D/') if not f.startswith(tuple(prefixes))), key=str.lower)
-
-        for i in range(len(list_spec)):
-
-            # Load the flux sampling template
-            sampling_path = '/Users/saraswati/Documents/Work/spec-uao/calibration_star/BD+33d2642_220230415_sampling.fits'
-            sampling = Table.read(sampling_path)
-
-            # Load the .FITS file
-            hdul = fits.open(reduced+spectra[0]+'/obj_abs_1D/'+list_spec[i])
-            name = hdul[0].header['OBJECT']
-            name_clean = name.rsplit('_', 1)[0]
-
-            # Load the observed spectrum
-            spec_obs_path = '/Users/saraswati/Documents/Work/spec-uao/clean_spec/'+name_clean+'.fits'
-            spec_obs = Table.read(spec_obs_path)
-
-            # create a new wavelength array for both spectra
-            new_wav = spec_obs['WAVELENGTH']
-            flux = spec_obs['FLUX']
-
-            # resample the 'sampling' spectra based on the new wavelength array
-            flux_sampling_resample = spectres(new_wav, sampling['WAVELENGTH'], sampling['FLUX'], verbose=False)
-
-            # Apply the flux sampling to the observed spectra
-            flux_calibrated = spec_obs['FLUX'] * flux_sampling_resample
-
-            #smooth the spectra
-            N = 5
-            spec_convolve = convolve(flux_calibrated, np.ones(N), boundary='extend', normalize_kernel=True)
-
-            #save spectra into a DataFrame
-            spec_new = pd.DataFrame()
-            spec_new.insert(0, "WAVELENGTH", new_wav)
-            spec_new.insert(1, "FLUX", spec_convolve)
-
-            #convert DataFrame into Table
-            spec_tab = Table.from_pandas(spec_new)
-
-            #save each spectrum into clean_spec directory
-            os.chdir(calibrated_spec)
-            spec_tab.write(name_clean+'.fits')
-
-    elif folder[i] == 'Hviding2023A_2_735':
-
-        #list the .FITS file for each folder
-        prefixes = ["slitA001_1", "slitA015_1", "slitB002_1", "slitB011_1"]
-        list_spec = sorted((f for f in os.listdir(reduced+spectra[0]+'/obj_abs_1D/') if not f.startswith(tuple(prefixes))), key=str.lower)
-    
-        for i in range(len(list_spec)):
-
-            # Load the flux sampling template
-            sampling_path = '/Users/saraswati/Documents/Work/spec-uao/calibration_star/BD+33d2642_20230708_sampling.fits'
-            sampling = Table.read(sampling_path)
-
-            # Load the .FITS file
-            hdul = fits.open(reduced+spectra[0]+'/obj_abs_1D/'+list_spec[i])
-            name = hdul[0].header['OBJECT']
-            name_clean = name.rsplit('_', 1)[0]
-
-            # Load the observed spectrum
-            spec_obs_path = '/Users/saraswati/Documents/Work/spec-uao/clean_spec/'+name_clean+'.fits'
-            spec_obs = Table.read(spec_obs_path)
-
-            # create a new wavelength array for both spectra
-            new_wav = spec_obs['WAVELENGTH']
-            flux = spec_obs['FLUX']
-
-            # resample the 'sampling' spectra based on the new wavelength array
-            flux_sampling_resample = spectres(new_wav, sampling['WAVELENGTH'], sampling['FLUX'], verbose=False)
-
-            # Apply the flux sampling to the observed spectra
-            flux_calibrated = spec_obs['FLUX'] * flux_sampling_resample
-
-            #smooth the spectra
-            N = 5
-            spec_convolve = convolve(flux_calibrated, np.ones(N), boundary='extend', normalize_kernel=True)
-
-            #save spectra into a DataFrame
-            spec_new = pd.DataFrame()
-            spec_new.insert(0, "WAVELENGTH", new_wav)
-            spec_new.insert(1, "FLUX", spec_convolve)
-
-            #convert DataFrame into Table
-            spec_tab = Table.from_pandas(spec_new)
-
-            #save each spectrum into clean_spec directory
-            os.chdir(calibrated_spec)
-            spec_tab.write(name_clean+'.fits')
-
-    elif folder[i] == 'Hviding2023A_4_737':
-
-        #list the .FITS file for each folder
-        prefixes = ["slitA001_1", "slitA015_1", "slitB002_1", "slitB011_1"]
-        list_spec = sorted((f for f in os.listdir(reduced+spectra[0]+'/obj_abs_1D/') if not f.startswith(tuple(prefixes))), key=str.lower)
-
-        for i in range(len(list_spec)):
-
-            # Load the flux sampling template
-            sampling_path = '/Users/saraswati/Documents/Work/spec-uao/calibration_star/BD+33d2642_20230708_sampling.fits'
-            sampling = Table.read(sampling_path)
-
-            # Load the .FITS file
-            hdul = fits.open(reduced+spectra[0]+'/obj_abs_1D/'+list_spec[i])
-            name = hdul[0].header['OBJECT']
-            name_clean = name.rsplit('_', 1)[0]
-
-            # Load the observed spectrum
-            spec_obs_path = '/Users/saraswati/Documents/Work/spec-uao/clean_spec/'+name_clean+'.fits'
-            spec_obs = Table.read(spec_obs_path)
-
-            # create a new wavelength array for both spectra
-            new_wav = spec_obs['WAVELENGTH']
-            flux = spec_obs['FLUX']
-
-            # resample the 'sampling' spectra based on the new wavelength array
-            flux_sampling_resample = spectres(new_wav, sampling['WAVELENGTH'], sampling['FLUX'], verbose=False)
-
-            # Apply the flux sampling to the observed spectra
-            flux_calibrated = spec_obs['FLUX'] * flux_sampling_resample
-
-            #smooth the spectra
-            N = 5
-            spec_convolve = convolve(flux_calibrated, np.ones(N), boundary='extend', normalize_kernel=True)
-
-            #save spectra into a DataFrame
-            spec_new = pd.DataFrame()
-            spec_new.insert(0, "WAVELENGTH", new_wav)
-            spec_new.insert(1, "FLUX", spec_convolve)
-
-            #convert DataFrame into Table
-            spec_tab = Table.from_pandas(spec_new)
-
-            #save each spectrum into clean_spec directory
-            os.chdir(calibrated_spec)
-            spec_tab.write(name_clean+'.fits')
+        #save each spectrum into clean_spec directory
+        os.chdir(calibrated_spec)
+        spec_tab.write(name_clean+'.fits')
