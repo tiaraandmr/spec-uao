c98754f86d6423fbad9ea8f1969915ff0a0c23a5
Tiara Andamari Saraswati <tiaraandmr@users.noreply.github.com>
2024-08-12 16:02:32 +0200

    Update line ratio calculation

--- line_measurement.py
+++ line_measurement.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 # Import packages
+import os
 import numpy as np
+import pandas as pd
 from astropy.table import Table
 from matplotlib import rc
 rc('font',**{'family':'serif','serif':['lmodern']})
@@ -9,49 +11,116 @@ rc('text', usetex=True)
 
 import matplotlib.pyplot as plt
 from astropy.io import fits
-import argparse
 
-# Define parser
-parser = argparse.ArgumentParser()
-parser.add_argument('--id', default='41231836365464984')
-args = parser.parse_args()
-results_id = args.id
+# Open results file
+home = '/Users/saraswati/Documents/Work/spec-uao/'
+results_path = home+'results/GELATO-results.fits'
+results = Table.read(results_path, 1)
 
-# Define path to .FITS file
-results_path = f'/Users/saraswati/Documents/Work/spec-uao/results/{results_id}-results.fits'
-results = fits.open(results_path)
+ratio_1 = pd.Series([], dtype='float64')
+ratio_1_err = pd.Series([], dtype='float64')
+ratio_2 = pd.Series([], dtype='float64')
+ratio_2_err = pd.Series([], dtype='float64')
+ratio_3 = pd.Series([], dtype='float64')
+ratio_3_err = pd.Series([], dtype='float64')
+name_clean = pd.Series([], dtype='int')
+rest_color_val = pd.Series([], dtype='float64')
+rest_color_err = pd.Series([], dtype='float64')
 
-#.FITS data
-data = results[2].data
+# Open rest frame color file
+rest_color_path = home+'SED-concat.fits'
+rest_color = Table.read(rest_color_path,1)
 
-#.FITS header
-header = results[2].header
+for i in range(len(results)):
 
-# Line flux value
-ne_III = np.average(data['AGN_[NeIII]_3869.86_Flux'])
-o_II = np.average(data['SF_[OII]_3728.48_Flux'])
-o_III = np.average(data['AGN_[OIII]_5008.24_Flux'])
-h_beta = np.average(data['Balmer_HI_4862.68_Flux'])  
-#n_II = np.average(data['AGN_[NII]_6585.27_Flux'])
-#h_alpha = np.average(data['Balmer_HI_6564.61_Flux'])
+    # Line flux value
+    ne_III = results['AGN_[NeIII]_3869.86_Flux'][i]
+    ne_III_err = results['AGN_[NeIII]_3869.86_Flux_err'][i]
+    o_II = results['SF_[OII]_3728.48_Flux'][i]
+    o_II_err = results['SF_[OII]_3728.48_Flux_err'][i]
+    o_III = results['AGN_[OIII]_5008.24_Flux'][i]
+    o_III_err = results['AGN_[OIII]_5008.24_Flux_err'][i]
+    h_beta = results['Balmer_HI_4862.68_Flux'][i] 
+    h_beta_err = results['Balmer_HI_4862.68_Flux_err'][i]
+    n_II = results['AGN_[NII]_6585.27_Flux'][i]
+    n_II_err = results['AGN_[NII]_6585.27_Flux_err'][i]
+    h_alpha = results['Balmer_HI_6564.61_Flux'][i]
+    h_alpha_err = results['Balmer_HI_6564.61_Flux_err'][i]
 
-# Line width value
-o_III_w = np.average(data['AGN_[OIII]_5008.24_REW'])
-#h_alpha_w = np.average(data['Balmer_HI_6564.61_REW'])
+    # List the line
+    lines_1 = [ne_III, o_II] 
+    lines_2 = [o_III, h_beta]
+    lines_3 = [n_II, h_alpha]
 
-# Line dispersion value
-o_III_d = np.average(data['AGN_[OIII]_5008.24_Dispersion'])
-#h_alpha_d = np.average(data['Balmer_HI_6564.61_Dispersion'])
+    for j in range(len(lines_1)):
+        if lines_1[0] != 'nan' and lines_1[1] != 'nan':
+            if lines_1[0] > 0 and lines_1[1] > 0:
+                ratio_1[i] = np.log10(ne_III/o_II)
+                ratio_1_err[i] = 0.434 * np.sqrt((ne_III_err/ne_III)+(o_II_err/o_II))
+            else:
+                ratio_1[i] = np.nan
+                ratio_1_err[i] = np.nan
+        else:
+            ratio_1[i] = np.nan
+            ratio_1_err[i] = np.nan
 
-ratio_1 = np.log10(ne_III/o_II)
-ratio_2 = np.log10(o_III/h_beta)
-#ratio_3 = np.log10(n_II/h_alpha)
+    for k in range(len(lines_2)):
+        if lines_2[0] != 'nan' and lines_2[1] != 'nan':
+            if lines_2[0] > 0 and lines_2[1] > 0:
+                ratio_2[i] = np.log10(o_III/h_beta)
+                ratio_2_err[i] = 0.434 * np.sqrt((o_III_err/o_III)+(h_beta_err/h_beta))
+            else:
+                ratio_2[i] = np.nan
+                ratio_2_err[i] = np.nan
+        else:
+            ratio_2[i] = np.nan
+            ratio_2_err[i] = np.nan
+    
+    for l in range(len(lines_3)):
+        if lines_3[0] != 'nan' and lines_3[1] != 'nan':
+            if lines_3[0] > 0 and lines_3[1] > 0:
+                ratio_3[i] = np.log10(n_II/h_alpha)
+                ratio_3_err[i] = 0.434 * np.sqrt((n_II_err/n_II)+(h_alpha_err/h_alpha))
+            else:
+                ratio_3[i] = np.nan
+                ratio_3_err[i] = np.nan
+        else:
+            ratio_3[i] = np.nan
+            ratio_3_err[i] = np.nan
+    
+    name_clean[i] = results['Name'][i].rsplit('.', 1)[0]
 
-print(results_id)
-print(r'LOG([NeIII]/[OII]) = ', ratio_1)
-print(r'LOG([OIII]/H-beta) = ', ratio_2)
-#print(r'LOG([NII]/H-alpha) = ', ratio_3)
-print(r'[OIII] REW = ', o_III_w)
-#print(r'H_alpha REW = ', h_alpha_w)
-print(r'[OIII] Dispersion = ', o_III_d)
-#print(r'H_alpha Dispersion = ', h_alpha_d)
+    for m in range(len(rest_color)):
+        if int(rest_color['ID'][m]) == int(name_clean[i]):
+            rc_z = rest_color['rest_sdss_z'][m]
+            err_rc_z = rest_color['rest_sdss_z_err'][m]
+            rc_g = rest_color['rest_sdss_g'][m]
+            err_rc_g = err_rc_z = rest_color['rest_sdss_g_err'][m]
+            rest_color_val[i] = 2.5 * np.log10(rc_z/rc_g)
+            rest_color_err[i] = 2.5 * 0.434 * np.sqrt((err_rc_z/rc_z)+(err_rc_g/rc_g))
+        else:
+            pass
+
+#save value into a DataFrame
+ratio = pd.DataFrame()
+ratio.insert(0, "ID", name_clean)
+ratio.insert(1, "Ne_III/O_II_Flux_Ratio", ratio_1)
+ratio.insert(2, "Ne_III/O_II_Flux_Ratio_Err", ratio_1_err)
+ratio.insert(3, "O_III/H_Beta_Flux_Ratio", ratio_2)
+ratio.insert(4, "O_III/H_Beta_Flux_Ratio_Err", ratio_2_err)
+ratio.insert(5, "N_II/H_Alpha_Flux_Ratio", ratio_3)
+ratio.insert(6, "N_II/H_Alpha_Flux_Ratio_Err", ratio_3_err)
+ratio.insert(7, "Rest_SDSS_Color", rest_color_val)
+ratio.insert(8, "Rest_SDSS_Color_Err", rest_color_err)
+
+#ratio_tab = Table([name_clean, ratio_1, ratio_1_err, ratio_2, ratio_2_err,
+#                   ratio_3, ratio_3_err, rest_color_val, rest_color_err], 
+#                   names=('ID', 'Ne_III/O_II_Flux_Ratio', 'Ne_III/O_II_Flux_Ratio_Err', 
+#                          'O_III/H_Beta_Flux_Ratio', 'O_III/H_Beta_Flux_Ratio_Err', 'N_II/H_Alpha_Flux_Ratio',
+#                          'N_II/H_Alpha_Flux_Ratio_Err', 'Rest_SDSS_Color', 'Rest_SDSS_Color_Err'))
+
+#convert DataFrame into Table
+ratio_tab = Table.from_pandas(ratio)
+
+os.chdir(home+'results')
+ratio_tab.write('line_ratio.fits', overwrite=True)
