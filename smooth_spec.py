01637667c622657571ee3363e5b9c643009476a2
tiaraandmr <srsvvt@gmail.com>
2024-09-07 22:55:31 +0700

    Update script

--- smooth_spec.py
+++ smooth_spec.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python python3
+#!/usr/bin/python3
 
 import os, glob, shutil, subprocess as sub
 from pathlib import Path
@@ -13,43 +13,46 @@ rc('text', usetex=True)
 import matplotlib.pyplot as plt
 from astropy.io import fits
 
-clean_spec = '/Users/saraswati/Documents/Work/spec-uao/clean_spec/'
+# Define source folder
+clean_spec = '/home/tiara/Documents/Work/spec-uao/clean_spec/'
 
-smooth_spec = '/Users/saraswati/Documents/Work/spec-uao/smooth_spec/'
+# Create new folder for the results
+smooth_spec = '/home/tiara/Documents/Work/spec-uao/smooth_spec/'
 if os.path.exists(smooth_spec):
     shutil.rmtree(smooth_spec)
 os.makedirs(smooth_spec)
 
-#define line species
+# Define line species
 species = np.array([['[SII]', 6716.435709204644], ['[SII]', 6730.811841618589], ['[NII]', 6583.451474377235], ['[NII]', 6548.050991532048], 
                     ['[OIII]', 5006.843330296288], ['[OIII]', 4958.91106815638], ['[OIII]', 4363.2096874449635], ['[NeIII]', 3868.7632032493057], 
                     ['[NeV]', 3425.8676240424247], ['[NeV]', 3345.828076914398], ['[OI]', 6300.303551730963], ['[OI]', 6363.776506749638], 
                     ['[OII]', 3727.4199178007175], ['HI', 6562.797027356974], ['HI', 4861.321979760415], ['HI', 4340.459677187083], 
                     ['CIV', 1548.8576020874154], ['HeII', 1639.7908206670402], ['CIII]', 1908.1020362243723], ['MgII', 2798.2921038031536]])
 
-slits = Table.read('/Users/saraswati/Documents/Work/spec-uao/slits_reduced.fits')
+# Define object ID and preliminary z
+slits = Table.read('/home/tiara/Documents/Work/spec-uao/slits_reduced.fits')
 object_ID = slits['ID'].astype('str')
 z = slits['BEST_Z']
 
 for i in range(len(object_ID)):
-    #read the spectrum
+    # Read the spectrum
     spec_data = Table.read(clean_spec+object_ID[i]+'.fits')
-    wav = spec_data['WAVELENGTH']
+    wav = spec_data['WAVE']
     flux = spec_data['FLUX']
     
-    #smooth the spectra
+    # Smooth the spectra with N=5
     N = 5
     spec_convolve = convolve(flux, np.ones(N), boundary='extend', normalize_kernel=True)
     good = np.invert(np.isnan(flux))
 
-    #define the figure and font size
+    # Define the figure and font size
     plt.rcParams.update({'font.size': 17})
     fig, ax = plt.subplots(figsize=(17,5))
 
-    #plot the spectrum and set the x-limit
+    # Plot the spectrum and set the x-limit
     ax.plot(wav, spec_convolve, color='firebrick', linewidth=2.0, drawstyle='steps-mid')
     
-    #define the approximate redshift (z)
+    # Define the approximate redshift (z)
     best_z = z[i]
 
     #check if lines are in the range of the observed spectrum
