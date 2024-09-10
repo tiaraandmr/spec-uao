3aa7808366ec82d217a0d99401e24be0e6848115
tiaraandmr <srsvvt@gmail.com>
2024-09-10 21:47:52 +0700

    Update scripts

--- flux_calib_all.py
+++ flux_calib_all.py
@@ -16,11 +16,12 @@ import pandas as pd
 rc('font', **{'family': 'serif', 'serif': ['lmodern']})
 rc('text', usetex=True)
 
-#define path to the .FITS file
+# Define path to the .FITS file
 home = '/Users/saraswati/Documents/Work/spec-uao/UAO-S118-23A/'
 exclude = ["BD", "Feige"]
 folder = sorted((f for f in os.listdir(home) if not f.startswith(tuple(exclude))), key=str.lower)
 
+# Create new folder for the results
 calibrated_spec = '/Users/saraswati/Documents/Work/spec-uao/calibrated_spec/'
 if os.path.exists(calibrated_spec):
     shutil.rmtree(calibrated_spec)
@@ -33,7 +34,7 @@ os.makedirs(fit_spec)
 
 for f in folder:
 
-    #list the folder inside home directory
+    # List the folder inside home directory
     reduced = home+f+'/reduced/'
     spectra = sorted(os.listdir(reduced))
 
@@ -48,7 +49,7 @@ for f in folder:
     else:
         continue
 
-    #list the .FITS file for each folder
+    # List the .FITS file for each folder
     prefixes = ["slitA001_1", "slitA015_1", "slitB002_1", "slitB011_1"]
     list_spec = sorted((f for f in os.listdir(reduced+spectra[0]+'/obj_abs_1D/') if not f.startswith(tuple(prefixes))), key=str.lower)
 
@@ -66,13 +67,13 @@ for f in folder:
         spec_obs_path = '/Users/saraswati/Documents/Work/spec-uao/clean_spec/'+name_clean+'.fits'
         spec_obs = Table.read(spec_obs_path)
 
-        # create a new wavelength array for both spectra
+        # Create a new wavelength array for both spectra
         new_wav = spec_obs['WAVE']
         flux = spec_obs['FLUX']
         err = spec_obs['ERROR']
         mask = spec_obs['MASK']
 
-        # resample the 'sampling' spectra based on the new wavelength array
+        # Resample the 'sampling' spectra based on the new wavelength array
         flux_sampling_resample, flux_err_sampling_resample = spectres(new_wav, sampling['WAVE'], sampling['FLUX'], spec_errs=sampling['ERROR'], verbose=False)
 
         # Apply the flux sampling to the observed spectra
@@ -82,21 +83,19 @@ for f in folder:
 
         spec_tab = Table([new_wav, flux_calibrated, flux_err_calibrated, mask], names=('WAVE', 'FLUX', 'ERROR', 'MASK'))
        
-        #save each spectrum into calibrated_spec directory
+        # Save each spectrum into calibrated_spec directory
         os.chdir(calibrated_spec)
         spec_tab.write(name_clean+'.fits')
 
-        # fits table for fitting purpose
+        # Fits table for fitting purpose
         wav_fit = np.log10(new_wav)
-        #wav_fit = np.log10(spec_obs['WAVE'])
         flux_fit = flux_calibrated
-        #flux_fit = spec_obs['FLUX']
         ivar_fit = 1 / np.sqrt(flux_err_calibrated)
-        #ivar_fit = 1 / np.sqrt(spec_obs['ERROR'])
 
+        # Convert array into Table
         spec_fit = Table([wav_fit, flux_fit, ivar_fit], names=('loglam', 'flux', 'ivar'))
 
-        #save each spectrum into fit_spec directory
+        # Save each spectrum into fit_spec directory
         os.chdir(fit_spec)
         spec_fit.write(name_clean+'.fits')
 
